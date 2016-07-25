199e71b5b2ff819e1a56a1b7271e4c1304e5db49
Shifu Chen <chen@haplox.com>
2016-07-25 14:59:08 +0800

    support testing with mutscan test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include "fastqreader.h"
+#include "unittest.h"
 
 int main(int argc, char* argv[]){
+    if (argc == 2 && strcmp(argv[1], "test")==0){
+        UnitTest tester;
+        tester.run();
+        return 0;
+    }
 	if(argc<3){
-		printf("usage:\n\tmutscan <read1_file> <read2_file>\n");
+		printf("Usage:\n\tmutscan <read1_file> <read2_file>\n");
+        printf("Testing:\n\tmutscan test\n");
 		exit(1);
 	}
 }
--- src/unittest.cpp
+++ src/unittest.cpp
@@ -31,7 +31,7 @@ bool UnitTest::editdistance(){
     for(int i=0;i<3;i++){
         int ret  = edit_distance(str1[i], strlen(str1[i]), str2[i], strlen(str2[i]));
         if(ret != result[i]){
-            printf("Fail: (edit_distance_dp), expect %d, but got %d, \n%s\n%s\n", result[i], ret, str1[i], str2[i]);
+            printf("Fail: (edit_distance_dp), expect %d, but got %d: \n%s\n%s\n", result[i], ret, str1[i], str2[i]);
             return false;
         }
     }
