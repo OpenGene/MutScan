9c13e9fe067687568c0352f3bb9b3cfc19c37e78
Shifu Chen <chen@haplox.com>
2016-08-03 12:52:14 +0800

    add time using stat

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "fastqreader.h"
 #include "unittest.h"
 #include "mutscan.h"
+#include <time.h>
 
 int main(int argc, char* argv[]){
     if (argc == 2 && strcmp(argv[1], "test")==0){
@@ -14,9 +15,12 @@ int main(int argc, char* argv[]){
         printf("Testing:\n\tmutscan test\n");
 		exit(1);
 	}
+    clock_t t1 = clock();
     string mutationFile(argv[1]);
     string r1file(argv[2]);
     string r2file(argv[3]);
     MutScan scanner(mutationFile, r1file, r2file);
     scanner.scan();
+    clock_t t2 = clock();
+    printf("\nTime used: %f ms\n", (t2-t1)/1000.0);
 }
