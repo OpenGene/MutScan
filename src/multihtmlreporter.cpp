f3f93e6754c8b4c6e744e6865195b06cc26ec261
Shifu Chen <chen@haplox.com>
2017-03-26 08:18:31 +0800

    mkdir chr folder first

--- src/multihtmlreporter.cpp
+++ src/multihtmlreporter.cpp
@@ -33,8 +33,8 @@ void MultiHtmlReporter::stat(){
 }
 
 void MultiHtmlReporter::run() {
-    printMutationHtml();
     printChrHtml();
+    printMutationHtml();
     printMainFrame();
     printIndexPage();
     printMainPage();
